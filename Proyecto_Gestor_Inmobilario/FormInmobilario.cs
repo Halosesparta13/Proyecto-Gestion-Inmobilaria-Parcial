7bd7391e61cd030e610581f76b73e71808a40043
aleney <149546025+aleney@users.noreply.github.com>
2024-10-10 15:51:53 -0500

    Update FormInmobilario.cs

--- Proyecto_Gestor_Inmobilario/FormInmobilario.cs
+++ Proyecto_Gestor_Inmobilario/FormInmobilario.cs
@@ -15,6 +15,78 @@ namespace Proyecto_Gestor_Inmobilario
         public FormInmobilario()
         {
             InitializeComponent();
+                        MostrarPropiedades(inmobiliarioServicio.ListarTodo());
         }
+
+        private void MostrarPropiedades(List<Inmobiliario> inmobiliarios)
+        {
+            dtgInmobiliario.DataSource = null;
+            if (inmobiliarios.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                dtgInmobiliario.DataSource = inmobiliarios;
+            }
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            //Validación de campos
+            if (txtNombre.Text == "" || txtUbicacion.Text == "" || txtPago.Text == "" || cmbTipo.Text == ""||txtDescripcion.Text == ""||txtAgregados.Text == "")
+            {
+                MessageBox.Show("Ingrese todos los campos");
+                return;
+            }
+            //Creacion de objetos
+            Inmobiliario inmobiliario = new Inmobiliario()
+            {
+                NombrePropiedad = txtNombre.Text,
+                Ubicación = txtUbicacion.Text,
+                PagoMensual = int.Parse(txtPago.Text),
+                DescipciónPropiedad = txtDescripcion.Text,
+                TipoInmueble = cmbTipo.Text,
+                ServicioAgregados = txtAgregados.Text,
+            };
+            //No se repite
+            bool registrado = inmobiliarioServicio.Registrar(inmobiliario);
+            if (!registrado)
+            {
+                MessageBox.Show("Debe de ingresar un registro diferente");
+                return;
+            }
+            //Mostrar
+            MostrarPropiedades(inmobiliarioServicio.ListarTodo());
+        }
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //validar selección
+            if (dtgInmobiliario.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Seleccione un registro");
+                return;
+            }
+            string codigo = dtgInmobiliario.SelectedRows[0].Cells[0].ToString();
+            inmobiliarioServicio.Eliminar(codigo);
+            MostrarPropiedades(inmobiliarioServicio.ListarTodo());
+        }
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            // Limpiar la lista del servicio
+            inmobiliarioServicio.LimpiarLista(); 
+            MostrarPropiedades(inmobiliarioServicio.ListarTodo());
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormInmobilario_Load(object sender, EventArgs e)
+        {
+
+        }
+    
     }
 }
