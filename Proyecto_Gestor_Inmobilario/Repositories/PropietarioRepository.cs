21fcc388b1124969189fbd71ff4eb7573bb4de42
Halosesparta <64061466+Halosesparta13@users.noreply.github.com>
2024-10-10 19:30:25 -0500

    Creando repositories

---
+++ Proyecto_Gestor_Inmobilario/Repositories/PropietarioRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Gestor_Inmobilario.Repositories
+{
+    internal class PropietarioRepository
+    {
+    }
+}
