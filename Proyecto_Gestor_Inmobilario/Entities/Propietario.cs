674cf43be4a907afdde441f93ed0e34840f39c61
Halosesparta <64061466+Halosesparta13@users.noreply.github.com>
2024-10-10 19:56:09 -0500

    Agregar entety Propietario, Inmobilario

---
+++ Proyecto_Gestor_Inmobilario/Entities/Propietario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Gestor_Inmobilario.Entities
+{
+    internal class Propietario
+    {
+        public Propietario() { }
+        public int Propietario_Id { get; set; }
+        public string Nombre_Usuario { get; set; }
+        public string Nombre_Completo { get; set; }
+        public string DNI { get; set; }
+        public string Correo { get; set; }
+        public string contraseña { get; set; }
+        public string Celular { get; set; }
+        public string RUC { get; set; }
+    }
+}
